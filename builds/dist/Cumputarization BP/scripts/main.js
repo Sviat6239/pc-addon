f394594520be135bf7a37016edf74727c0837b79
Sviatoslav <sviatoslav6239@gmail.com>
2026-06-26 21:46:02 +0200

    Delete builds directory

--- builds/dist/Cumputarization BP/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-
-world.afterEvents.worldInitialize.subscribe(() => {
-    console.warn("Мод Computarization успешно запущен!");
-});
-
-world.afterEvents.playerSpawn.subscribe((event) => {
-    event.player.sendMessage("Система Computarization: Ядро инициализировано.");
-});
